6c2c8c0d01ec366b6b80753fa92930498ad9e0d2
coazz <aco94@hotmail.com>
2023-02-21 22:12:02 +0100

    upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,41 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Container } from "react-bootstrap"
+import { Routes, Route, Navigate } from "react-router-dom"
+import { NewNote } from "./NewNote"
+
+export type Note = {
+    id: string
+} & NoteData
+
+export type NoteData = {
+    title: string
+    markdown: string
+    tags: Tag[]
+}
+
+export type Tag = {
+    id: string
+    label: string
+}
+
 export function App() {
     return (
-        <div>
-            <h1>Test</h1>
-        </div>
+        <Container className="my-4">
+            <Routes>
+                // Home Route
+                <Route path="/" element={<h1>Home</h1>} />
+                // make a new Note
+                <Route path="/new" element={<NewNote />} />
+                // Id of the note
+                <Route path="/:id">
+                    // show this always at first
+                    <Route index element={<h1>Show</h1>} />
+                    // Access the edit page through the Notes id page
+                    <Route path="edit" element={<h1>Edit</h1>} />
+                </Route>
+                // all other pages will be redirected to the Home Scree again
+                <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+        </Container>
     )
 }
